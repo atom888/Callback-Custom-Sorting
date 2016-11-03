b1fc4d2d80717a3162ddfba6724b2cfb9fa03f4e
Adam Dipinto <adam.dipinto@gmail.com>
2016-11-03 20:17:58 +0000

    Completed sorting numerically and in ascending order.

--- callback-custom-sorting.js
+++ callback-custom-sorting.js
@@ -1,4 +1,12 @@
 // Lighthouse Lab - W1D4 - Exercise - Callbacks - Custom Sorting //
 
+
+//Sort numerically and ascending:
 var array = [10, 2, 5, 1, 9];
-array.sort();
+array.sort(function(a,b) {
+  return a - b;
+});
+
+console.log(array);
+
+////
